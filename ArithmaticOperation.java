c24142553f583b67c7789e3698d714e2654748b3
jeevithajeevitha3420-create <jeevithajeevitha3420@gmail.com>
2026-03-28 09:38:26 +0530

    Add files via upload

---
+++ ArithmaticOperation.java
@@ -0,0 +1,12 @@
+class ArithmaticOperation
+{
+	public static void main(String args[])
+	{
+	int a = 19;
+	int b = 20;
+	System.out.println(a+b);
+	System.out.println(a-b);
+	System.out.println(a*b);
+	System.out.println(a/b);
+	}
+}
